bc44a029b5af7760388cc588755dd4ed1cd1dfdc
Elgur <leroy.maxence@yahoo.com>
2019-09-28 20:23:20 +0200

    added sun/mon button to hmtl css and js for darkmode. still not working

--- letrombi.js
+++ letrombi.js
@@ -4,18 +4,17 @@ const darkMode = document.querySelector('.moon');
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 lightMode.onclick = function(){
-  document.documentElement.setAttribute('data-theme', 'dark');
-  lightMode.style = "display: none";
-  darkMode.style = "display: block";
-  localStorage.setItem('theme', 'dark');
+    document.documentElement.setAttribute('data-theme', 'dark');
+    lightMode.style = "display: none";
+    darkMode.style = "display: block";
+    localStorage.setItem('theme', 'dark');
 }
 
 darkMode.onclick = function(){
-  document.documentElement.setAttribute('data-theme', 'light');
-  lightMode.style = "display: block";
-  darkMode.style = "display: none";
-  localStorage.setItem('theme', 'light');
-  
+    document.documentElement.setAttribute('data-theme', 'light');
+    lightMode.style = "display: block";
+    darkMode.style = "display: none";
+    localStorage.setItem('theme', 'light');     
 }
 
 if (currentTheme) {
@@ -23,6 +22,6 @@ if (currentTheme) {
 
     if (currentTheme === 'dark') {
         lightMode.style = "display: none";
-      darkMode.style = "display: block";
+        darkMode.style = "display: block";
     }
 }
