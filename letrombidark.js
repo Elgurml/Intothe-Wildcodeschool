a7a55a8456299db02b04bf11532d7f61547fd29a
Elgur <leroy.maxence@yahoo.com>
2019-10-01 17:13:53 +0200

    Revert "divided letrombi.html to letrombi.html and letrombidark.html and added letrombidark.css and letrombidark.js"
    This reverts commit 9bf7f78d3c9339349359392d5cc1918fed613b57.

--- letrombidark.js
+++
@@ -1,7 +0,0 @@
-const darkMode = document.querySelector('.sun');
-
-darkMode.onclick = function(){
-    window.location.href = "letrombi.html"
-}
-
-// const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
