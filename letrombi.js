9bf7f78d3c9339349359392d5cc1918fed613b57
Elgur <leroy.maxence@yahoo.com>
2019-10-01 16:39:19 +0200

    divided letrombi.html to letrombi.html and letrombidark.html and added letrombidark.css and letrombidark.js

--- letrombi.js
+++ letrombi.js
@@ -1,27 +1,24 @@
-const lightMode = document.querySelector('.sun');
-const darkMode = document.querySelector('.moon');
-
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-
-lightMode.onclick = function(){
-    document.documentElement.setAttribute('data-theme', 'dark');
-    lightMode.style = "display: none";
-    darkMode.style = "display: block";
-    localStorage.setItem('theme', 'dark');
-}
+const darkMode = document.querySelector('.sun');
 
 darkMode.onclick = function(){
-    document.documentElement.setAttribute('data-theme', 'light');
-    lightMode.style = "display: block";
-    darkMode.style = "display: none";
-    localStorage.setItem('theme', 'light');     
+    window.location.href = "letrombidark.html"
 }
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+// const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-    if (currentTheme === 'dark') {
-        lightMode.style = "display: none";
-        darkMode.style = "display: block";
-    }
-}
+
+// darkMode.onclick = function(){
+//     document.documentElement.setAttribute('data-theme', 'light');
+//     lightMode.style = "display: block";
+//     darkMode.style = "display: none";
+//     localStorage.setItem('theme', 'light');     
+// }
+
+// if (currentTheme) {
+//     document.documentElement.setAttribute('data-theme', currentTheme);
+
+//     if (currentTheme === 'dark') {
+//         lightMode.style = "display: none";
+//         darkMode.style = "display: block";
+//     }
+// }
---
+++ letrombidark.js
@@ -0,0 +1,7 @@
+const darkMode = document.querySelector('.sun');
+
+darkMode.onclick = function(){
+    window.location.href = "letrombi.html"
+}
+
+// const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
